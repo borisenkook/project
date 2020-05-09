1f514283d83ccf86db5802aaf4e084fc03da437f
Olga <borisenko.ok@yandexx.ru>
2020-05-09 15:31:52 +0300

    all tasks of Homework1

---
+++ hw1/Homework1 task5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_task5
+{
+    class Program
+    {
+        // Борисенко О.К.
+        // Задание 5. а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+        // б) Сделать задание, только вывод организуйте в центре экрана
+        // в) *Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y)
+
+        static void Main()
+        {
+            Console.WriteLine(" Меня зовут Ольга Борисенко. Я живу в городе Санкт-Петербург.");
+
+            string str = " Меня зовут Ольга Борисенко. Я живу в городе Санкт-Петербург.";
+            Console.SetCursorPosition((Console.WindowWidth - str.Length) / 2, Console.WindowHeight / 2);
+            Console.WriteLine(str);
+            Console.WriteLine(Signature());
+            Console.ReadKey();
+        }
+        static string Signature()
+        {
+            return "Ольга Борисенко, Санкт-Петербург";
+        }
+        
+    }
+}
---
+++ hw1/Homework1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Autor "Борисенко О.К."
+            // Задание 1. Написать программу «Анкета». Последовательно задаются вопросы (имя, фамилия, возраст, рост, вес). В результате вся информация выводится в одну строчку.
+            // а) используя склеивание;
+            // б) используя форматированный вывод;
+            // в) *используя вывод со знаком $.
+
+            Console.WriteLine("Введите Ваше имя:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите Вашу фамилию:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Укажите Ваш возраст:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Укажите Ваш рост в сантиметрах:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Укажите Ваш вес в килограммах:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" " + name + " " + surname + " " + age + " лет, рост - " + height + " сантиметров, вес - " + weight + " килограммов");
+            Console.WriteLine(String.Format(" Меня зовут {0}. Фамилия - {1}. Мне {2} лет. Мой рост {3} сантиметров, вес - {4} килограммов.", name, surname, age, height, weight));
+            Console.WriteLine($" Меня зовут {name}. Фамилия - {surname}. Мне {age} лет. Мой рост {height} сантиметров, вес - {weight} килограммов.");
+            Console.ReadKey();
+
+        }
+    }
+}
