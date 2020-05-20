5fd273321f092faea43c082132167590c1cf20e0
Olga <borisenko.ok@yandexx.ru>
2020-05-20 21:52:46 +0300

    add hm5

---
+++ hw5/Homework5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    class Program
+    {
+        static void Signature()
+        {
+            Console.WriteLine("Ольга Борисенко, Санкт-Петербург");
+        }
+        // создаем метод проверки логина LoginKontrol без регулярных выражений
+        static public bool LoginKontrol(string a)
+        {
+            if ((a.Length < 2) || (a.Length > 10)) return false;
+            if (char.IsDigit(a[0])) return false;
+            for (int i = 1; i < a.Length; i++)
+                if (!char.IsLetterOrDigit(a[i])) return false;
+            return true;
+        }
+
+        // создаем метод проверки логина LoginKontrol на основе регулярных выражений
+        static public bool LoginKontrolReg(string b)
+        {
+            if ((b.Length < 2) || (b.Length > 10)) return false;
+            Regex regex = new Regex(@"^[A - Za - zА - Яа - яЁё] +[A - Za - zА - Яа - яЁё0 - 9]");
+            return regex.IsMatch(b);
+        }
+
+        static void Main(string[] args)
+        {
+            Signature();
+            Console.WriteLine("Задание 1. Создать программу, которая будет проверять корректность ввода логина."+
+                 "Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры,"+
+                 "при этом цифра не может быть первой:"+
+            "а) без использования регулярных выражений;"+
+            "б) **с использованием регулярных выражений.");
+                       
+            //Вводим логин
+            Console.WriteLine("Введите логин:");
+            string login = Console.ReadLine();
+
+            //Проверяем введенный логин методом LoginKontrol
+            if (LoginKontrol(login)) Console.WriteLine("Результат проверки 1: Логин установлен правильно");
+            else Console.WriteLine("Результат проверки 1: Логин не соответствует правилам");
+
+            //Проверяем введенный логин методом LoginKontrolReg
+            if (LoginKontrolReg(login)) Console.WriteLine("Результат проверки 2: Логин установлен правильно");
+            else Console.WriteLine("Результат проверки 2: Логин не соответствует правилам");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ hw5/Homework5_task2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5_task2
+{
+    class Message
+    {
+        string message;
+        public  string TextMessage { 
+            get { return this.message; }
+            set { this.message = value; }
+        } // объявляем автоматическое свойство 
+    }
+    class Program
+    {
+                
+        static void Signature()
+        {
+            Console.WriteLine("Ольга Борисенко, Санкт-Петербург");
+        }
+        
+        static void WordEqualLen(string mes, int len)// Выводим слова определенной длины
+        {
+            string[] words = mes.Split(' ');
+            foreach (string word in words)
+                if (word.Length == len)
+                    Console.WriteLine(word);
+        }
+        static string WordDel(string mes, char a) // удаляем слова, заканчивающиеся на определенный символ
+        {
+            string[] words = mes.Split(' ');
+            string newMes = "";
+            foreach (string word in words)
+                if (word[word.Length - 1] == 'a') newMes += word + " ";
+                return newMes;
+        }
+
+        static string WordMax(string mes)
+        {
+            string[] words = mes.Split(' ');
+            int max = 0;
+            string wordMax = " ";
+            foreach (string word in words)
+                if (word.Length > max){ 
+                    max = word.Length;
+                    wordMax += word; 
+                }
+                return wordMax;
+        }
+                static void Main(string[] args)
+        {
+            Signature();
+            Console.WriteLine("Задание 2. Разработать статический класс Message," +
+                "содержащий следующие статические методы для обработки текста:" +
+        "а) Вывести только те слова сообщения,  которые содержат не более n букв." +
+        "б) Удалить из сообщения все слова, которые заканчиваются на заданный символ." +
+        "в) Найти самое длинное слово сообщения." +
+        "г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения." +
+        "д) ***Создать метод, который производит частотный анализ текста.В качестве параметра" +
+        "в него передается массив слов и текст, в качестве результата метод возвращает" +
+        "сколько раз каждое из слов массива входит в этот текст." +
+        "Здесь требуется использовать класс Dictionary.");
+
+            Message mes = new Message();
+            Console.WriteLine("Введите текст:");
+            mes.TextMessage = Console.ReadLine();
+            Console.WriteLine(mes.TextMessage);
+
+            Console.WriteLine("Введите количество букв в слове:");
+            int len = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"Слова, в которых {len} букв:");
+            WordEqualLen(mes.TextMessage, len);
+
+            Console.WriteLine("Введите символ на конце слов, которые нужно удалить:");
+            char a = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine($"Отредактированная строка: {WordDel(mes.TextMessage, a)}");
+
+      
+
+            Console.WriteLine($"Самое длинное слово содержит: {WordMax(mes.TextMessage)} букв");
+            Console.ReadKey();
+        }
+    }
+
+}
